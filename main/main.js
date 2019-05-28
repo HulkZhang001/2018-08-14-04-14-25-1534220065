dc45562fbf87357f9aba5bc0a7fc0804ef20c65d
HulkZhang001 <530670864@qq.com>
2019-05-28 13:49:55 +0800

    made change05281349

--- main/main.js
+++ main/main.js
@@ -1,4 +1,22 @@
-module.exports = function main() {
-    console.log("Debug Info");
+module.exports = function main(a) {
+    
+	if (a=='910'){
+	console.log(
+	"._. ... ._.\n"+
+    "|_| ..| |.|\n"+
+	"..| ..| |_|\n");
+	}
+	if (a=='256'){
+	console.log(
+	"._. ._. ._.\n"+
+	"._| |_. |_.\n"+
+	"|_. ._| |_|\n");
+	}
+	if (a=='7'){
+	console.log(
+	"._.\n"+
+	"..|\n"+
+	"..|\n");
+	}
     return 'Hello World!';
 };
