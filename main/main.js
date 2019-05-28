8f13a9e74f75347114a3c083d289b477d1dfd845
HulkZhang001 <530670864@qq.com>
2019-05-28 13:54:47 +0800

    made change05281354

--- main/main.js
+++ main/main.js
@@ -1,22 +1,46 @@
 module.exports = function main(a) {
     
-	if (a=='910'){
-	console.log(
-	"._. ... ._.\n"+
-    "|_| ..| |.|\n"+
-	"..| ..| |_|\n");
-	}
-	if (a=='256'){
-	console.log(
-	"._. ._. ._.\n"+
-	"._| |_. |_.\n"+
-	"|_. ._| |_|\n");
-	}
-	if (a=='7'){
-	console.log(
-	"._.\n"+
-	"..|\n"+
-	"..|\n");
-	}
-    return 'Hello World!';
+	var lcd='';
+
+    //初始化数组
+
+    var lcd_number_row1 = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+
+    var lcd_number_row2 = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+
+    var lcd_number_row3 = ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'];
+
+    //判断输入是否为数字
+
+    if(parseInt(a)){
+
+        //将数字转换为字符串获取每一位数字
+
+        var temp=String(a);
+
+        //将每一位数字对应的lcd图案按行拼接
+
+        var lcd_row1=new Array(),lcd_row2=new Array(),lcd_row3=new Array();
+
+        for (let i = 0; i < temp.length; i++) {
+
+            lcd_row1.push(lcd_number_row1[parseInt(temp[i])]);
+
+            lcd_row2.push(lcd_number_row2[parseInt(temp[i])]);
+
+            lcd_row3.push(lcd_number_row3[parseInt(temp[i])]);
+
+        }
+
+        //拼接lcd
+
+        lcd=lcd_row1.join(' ')+'\n'+lcd_row2.join(' ')+'\n'+lcd_row3.join(' ')+'\n'
+
+
+
+    }
+
+
+
+    return lcd;
 };
